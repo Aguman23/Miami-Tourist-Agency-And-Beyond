aca08dc03301d2ef7dbc062c4a74f36d86a0eebf
adelaguardiab@gmail.com <adelaguardiab@gmail.com>
2023-10-08 11:22:44 -0400

    Html for Explore Mars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./pages/HomePage/HomePage";
+import ExploreMarsPage from "./pages/ExploreMarsPage/ExploreMarsPage";
+import "./App.scss"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/explore-mars' element={<ExploreMarsPage/>} />
       </Routes>
       <NavBar />
     </BrowserRouter>
---
+++ src/pages/ExploreMarsPage/ExploreMarsPage.js
@@ -0,0 +1,66 @@
+import './ExploreMarsPage.scss';
+
+function ExploreMarsPage() {
+    return (
+        <main className='main-background'>
+            
+            
+            <h2>Explore Mars</h2>
+            
+            
+            <p>Embark on a journey to the enigmatic Red Planet, where adventure beckons at your own pace. Choose the high-speed sprint for a thrilling arrival within hours, or indulge in a luxurious, leisurely cruise through space. Whichever you pick, the allure of Mars awaits.</p>
+        
+
+            <h3>Choose your speed:</h3>
+
+
+            <div>
+
+                <div>
+                    {/* <img className="" src="" /> */}
+                </div>
+                
+                <h3>Direct Route</h3>
+                
+                <div>
+                    <p>Select</p>
+                </div>
+
+            </div>
+
+            <div>
+
+                <div>
+                    {/* <img className="" src="" /> */}
+                </div>
+
+                <h3>Scenic Route</h3>
+
+                <div>
+                    <p>Select</p>
+                </div>
+
+            </div>
+        
+        <h3>More about Mars</h3>
+        
+        <p>Views</p>
+
+        <div>
+        </div>
+
+        <p>Diameter...........................6,791 km / 4,220 mi</p>
+        <p>Day Length.................................24 hrs 37 min</p>
+        <p>Force of Gravity...........................38% of Earth</p>
+        <p>Distance from Earth..........225Mkm / 140Mmi</p>
+        <p>Age..........................................4.5 billion years</p>
+
+        <div>
+            <p>Learn More</p>
+        </div>
+
+        </main>
+    )
+}
+
+export default ExploreMarsPage;
