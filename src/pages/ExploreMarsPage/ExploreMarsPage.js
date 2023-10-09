bd25e7cb13f10df7463121c682d361f97f6b315e
adelaguardiab@gmail.com <adelaguardiab@gmail.com>
2023-10-08 20:05:21 -0400

    Finished styling page 2

--- src/pages/ExploreMarsPage/ExploreMarsPage.js
+++ src/pages/ExploreMarsPage/ExploreMarsPage.js
@@ -1,65 +1,108 @@
 import './ExploreMarsPage.scss';
-import Info from "../../assets/icons/Info.svg"
+import info from "../../assets/icons/Info white.svg"
+import rightarrow from "../../assets/icons/Sec button arrow (2).svg"
+import marsimage from "../../assets/images/moons of mars nasa photo.webp"
 
 function ExploreMarsPage() {
     return (
         <main className='mars-explore__main-background'>
             
             <div  className='mars-explore__title-container'>
-            <h2 className="mars-explore__title">Explore Mars</h2>
+                <h2 className="mars-explore__title">Explore Mars</h2>
             </div>
             
             <p className="mars-explore__description">Embark on a journey to the enigmatic Red Planet, where adventure beckons at your own pace. Choose the high-speed sprint for a thrilling arrival within hours, or indulge in a luxurious, leisurely cruise through space. Whichever you pick, the allure of Mars awaits.</p>
         
 
-            <h3 className="mars-explore__subtitle">Choose your speed:</h3>
+            <h3 className="mars-explore__subtitle-1">Choose your speed:</h3>
 
 
             <div className="mars-explore__route-container-1">
 
-                <div>
-                    <Info />
+                <div className="format-container">
+                
+                    <div className="format-left">
+                        <h3 className="mars-explore__subtitle-2">Direct Route</h3>
+                    </div>
+
+                    <div  className="format-right">
+                        <img className="mars-explore__info" src={info} alt="info"/>
+                    </div>
+
                 </div>
                 
-                <h3>Direct Route</h3>
-                
-                <div>
-                    <p>Select</p>
+                <div className="format-bottom-right">
+                    <button className="mars-explore__select-button">Select</button>
+                    <img src={rightarrow} alt="right arrow"/>
                 </div>
 
             </div>
 
             <div className="mars-explore__route-container-2">
 
-                <div>
-                    {/* <img className="" src="" /> */}
+                <div className="format-container">
+
+                    <div className="format-left">
+                        <h3 className="mars-explore__subtitle-2">Scenic Route</h3>
+                    </div>
+
+                    <div className="format-right">
+                        <img src={info} alt="info"/>
+                    </div>
+
                 </div>
 
-                <div>
-                    <h3>Scenic Route</h3>
-                </div>
-
-                <div>
-                    <p>Select</p>
+                <div className="format-bottom-right">
+                    <button className="mars-explore__select-button">Select</button>
+                    <img src={rightarrow} alt="right arrow"/>
                 </div>
 
             </div>
         
-        <h3>More about Mars</h3>
+        <div className="mars-explore__buttons-container">
+            <div>
+                <button className="mars-explore__red-buttons">Previous</button>
+            </div>
+            <div>
+                <button className="mars-explore__red-buttons">Next</button>
+            </div>
+        </div>
+        <h3 className="mars-explore__more">More about Mars</h3>
         
-        <p>Views</p>
+        <p className="mars-explore__description-2">Views</p>
 
-        <div>
+        <img className="mars-explore__moon" src={marsimage} alt="mars"/>
+
+        <p className="mars-explore__description-2">Martian Facts</p>
+
+        <div className="format-facts">
+        <div><p className="mars-explore__description-3">Diameter</p></div>
+        <div><p className="mars-explore__description-3">6,791 km / 4,220 mi</p></div>
+        </div>
+        
+        <div className="format-facts">
+        <div><p className="mars-explore__description-3">Day Length</p></div>
+        <div><p className="mars-explore__description-3">24 hrs 37 min</p></div>
+        </div>
+        
+        <div className="format-facts">
+        <p className="mars-explore__description-3">Force of Gravity</p>
+        <p className="mars-explore__description-3">38% of Earth</p>
+        </div>
+        
+        <div className="format-facts">
+        <p className="mars-explore__description-3">Distance from Earth</p>
+        <p className="mars-explore__description-3">225Mkm / 140Mmi</p>
         </div>
 
-        <p>Diameter...........................6,791 km / 4,220 mi</p>
-        <p>Day Length.................................24 hrs 37 min</p>
-        <p>Force of Gravity...........................38% of Earth</p>
-        <p>Distance from Earth..........225Mkm / 140Mmi</p>
-        <p>Age..........................................4.5 billion years</p>
+        <div className="format-facts">
+        <p className="mars-explore__description-3">Age</p>
+        <p className="mars-explore__description-3">4.5 billion years</p>
+        </div>
 
-        <div>
-            <p>Learn More</p>
+        <div className="format__learn">
+                    <button className="mars-explore__select-button">LearnMore </button>
+                    <img src={rightarrow} alt="right arrow"/>
         </div>
 
         </main>
